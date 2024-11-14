b5f90b2f134ccb10bb88a8338ed848f75785fc1c
jessekl <jesse.klint.jk@gmail.com>
2024-11-15 01:58:22 +0200

    ShowtimesSection refactored

--- src/components/ShowtimesSection.js
+++ src/components/ShowtimesSection.js
@@ -5,7 +5,7 @@ import { XMLParser } from "fast-xml-parser";
 const ShowtimesSection = () => {
   const [showtimesData, setShowtimesData] = useState([])
 
-  const xmlToShowtimes = async (title, xml) => {
+  const xmlToJsonObj = async (xml) => {
     try {
       const options = {
         ignoreAttributes: false,
@@ -13,37 +13,21 @@ const ShowtimesSection = () => {
       const parser = new XMLParser(options);
       const xmlText = await xml.text();
       const jObj = parser.parse(xmlText);
-      const shows = jObj.Schedule.Shows.Show;
-
-      const filteredShows = shows
-        .filter((show) => (show.Title === title))
-        .map((show) => ({
-          id: show.ID,
-          time: show.dttmShowStart,
-          place: show.Theatre,
-        }));
-      return filteredShows;
+      return jObj;
     } catch (error) {
-      throw (error);
+      throw error;
     }
   }
 
   useEffect(() => {
     (async () => {
       try {
-        const testTitleIWantToShowTimeFor = "Gladiator II";
-
-        const cache = sessionStorage.getItem(testTitleIWantToShowTimeFor);
-        if (cache) {
-          setShowtimesData(JSON.parse(cache));
-          return;
-        }
         const res = await fetch(
           "https://www.finnkino.fi/xml/Schedule/"
         );
-        const parsedData = await xmlToShowtimes(testTitleIWantToShowTimeFor, res);
-        sessionStorage.setItem(testTitleIWantToShowTimeFor, JSON.stringify(parsedData));
-        setShowtimesData(parsedData);
+        const parsedData = await xmlToJsonObj(res);
+        const shows = parsedData.Schedule.Shows.Show;
+        setShowtimesData(shows);
       } catch (error) {
         console.log(error);
       };
@@ -53,8 +37,8 @@ const ShowtimesSection = () => {
   return (
     <div className="ShowtimesSection">
       <ul>
-        {showtimesData.map((show) => (
-          <Showtime key={show.id} data={show} />
+        {showtimesData.filter((show) => show.Title === "Gladiator II").map((show) => (
+          <Showtime key={show.ID} data={{ time: show.dttmShowStart, place: show.Theatre }} />
         ))}
       </ul>
     </div>
