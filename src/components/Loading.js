da7ceccc657a68042087b03ffea74acbb02f3b52
Nebul4s <j.hiltula24@gmail.com>
2024-11-19 14:11:50 +0200

    useFetch custom hook and refactored code to use it

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,9 @@
-const Loading = ({ item, children }) => {
-    const loading = () => {
-        return <div className="spinner" />
-    };
-    const loaded = () => {
-        return <div className="content">{children}</div>
-    };
-    return (
-        <div className="loading">{item.length > 0 ? loaded() : loading()}</div>
-    );
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div className="spinner" />
+    </div>
+  );
 };
 
 export default Loading;
