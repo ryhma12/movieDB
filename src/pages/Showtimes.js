d942d50b33405f4ae6756c7e50121fbe7f21cb9c
jessekl <jesse.klint.jk@gmail.com>
2024-11-25 20:50:04 +0200

    showtimes filtering by area

--- src/pages/Showtimes.js
+++ src/pages/Showtimes.js
@@ -1,16 +1,33 @@
-import ShowtimesSection from "../components/ShowtimesSection";
 import useXmlParse from "../hooks/useXmlParse";
 import useFetch from "../hooks/useFetch";
 import Showtime from "../components/Showtime";
 import Loading from "../components/utility/Loading";
+import Dropdown from "../components/Dropdown";
+import { useState } from "react";
 
 const Showtimes = () => {
+  const [selectedArea, setSelectedArea] = useState([]);
+  const [selectedAreaName, setSelectedAreaName] = useState("");
+
+  const handleTheatreArea = (index) => {
+    if (theatreAreaFetchData &&
+      !theatreAreaFetchError &&
+      theatreAreaParseData &&
+      !theatreAreaParseError) {
+      const currentArea = theatreAreaParseData[index + 1];
+      if (currentArea) {
+        setSelectedArea(currentArea.ID);
+        setSelectedAreaName(currentArea.Name);
+      };
+    };
+  };
+
   const {
     data: showtimeFetchData,
     error: showtimeFetchError,
     isLoading: showtimeIsLoading,
   } = useFetch(
-    `https://www.finnkino.fi/xml/Schedule?nrOfDays=31&area=1018`,
+    `https://www.finnkino.fi/xml/Schedule?nrOfDays=31&area=${selectedArea}`,
     "finnkino"
   );
 
@@ -20,31 +37,54 @@ const Showtimes = () => {
     isParsing: showtimeIsParsing,
   } = useXmlParse(showtimeFetchData, "Schedule.Shows.Show");
 
+  const {
+    data: theatreAreaFetchData,
+    error: theatreAreaFetchError,
+    isLoading: theatreAreaIsLoading,
+  } = useFetch(
+    `https://www.finnkino.fi/xml/TheatreAreas/`,
+    "finnkino"
+  );
+
+  const {
+    data: theatreAreaParseData = [],
+    error: theatreAreaParseError,
+    isParsing: theatreAreaIsParsing,
+  } = useXmlParse(theatreAreaFetchData, "TheatreAreas.TheatreArea");
+
   return (
-    <div className="ShowtimesSection">
-      {showtimeIsLoading || showtimeIsParsing ? (
-        <Loading />
-      ) : (
-        <ul>
-          {showtimeParseData ? (
-            showtimeParseData.map((show) => (
-              <Showtime
-                key={show.ID}
-                data={{
-                  movie: show.OriginalTitle,
-                  time: show.dttmShowStart,
-                  place: show.Theatre,
-                  spokenLang: show.SpokenLanguage?.ISOTwoLetterCode || null,
-                  subLang1: show.SubtitleLanguage1?.ISOTwoLetterCode || null,
-                  subLang2: show.SubtitleLanguage2?.ISOTwoLetterCode || null,
-                }}
-              />
-            ))
-          ) : (
-            <div className="empty">No showtimes found</div>
-          )}
-        </ul>
-      )}
+    <div className="showtimes">
+      <Dropdown
+        className="Dropdown"
+        options={theatreAreaParseData.slice(1).map((area) => (area.Name))}
+        handleSort={handleTheatreArea}
+        dropdownName={selectedAreaName ? `${selectedAreaName}` : "Filter by area"}
+      />
+      <div className="ShowtimesSection">
+        {showtimeIsLoading || showtimeIsParsing ? (
+          <Loading />
+        ) : (
+          <ul>
+            {showtimeParseData ? (
+              showtimeParseData.map((show) => (
+                <Showtime
+                  key={show.ID}
+                  data={{
+                    movie: show.OriginalTitle,
+                    time: show.dttmShowStart,
+                    place: show.Theatre,
+                    spokenLang: show.SpokenLanguage?.ISOTwoLetterCode || null,
+                    subLang1: show.SubtitleLanguage1?.ISOTwoLetterCode || null,
+                    subLang2: show.SubtitleLanguage2?.ISOTwoLetterCode || null,
+                  }}
+                />
+              ))
+            ) : (
+              <div className="empty">No showtimes found</div>
+            )}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
