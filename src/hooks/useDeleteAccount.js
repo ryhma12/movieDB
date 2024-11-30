45154dda8ddf2a7043b14f4b79bd6bfb28d755e5
Nebul4s <j.hiltula24@gmail.com>
2024-11-30 22:46:58 +0200

    General user settings and user deletion

--- src/hooks/useDeleteAccount.js
+++ src/hooks/useDeleteAccount.js
@@ -1,36 +1,39 @@
 import { useState } from "react";
 import { useUser } from "./useUser";
 
-export const useLogin = () => {
+export const useDeleteAccount = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const { dispatch } = useUser();
 
-  const login = async (email, password) => {
+  const deleteAccount = async (email, password, id) => {
     setError(null);
     setIsLoading(true);
 
     try {
-      const res = await fetch("http://localhost:3001/user/login", {
-        method: "POST",
+      const res = await fetch("http://localhost:3001/user/delete", {
+        method: "DELETE",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ Email: email, Password: password }),
+        body: JSON.stringify({
+          Password: password,
+          Email: email,
+          id: id,
+        }),
       });
-      if (!res.ok) {
-        setError("Login failed");
-      }
+      if (!res) throw new Error("Could not complete deletion");
 
       const data = await res.json();
       if (data.error) throw new Error(data.error);
 
-      dispatch({ type: "LOGIN", payload: data });
+      dispatch({ type: "LOGOUT", payload: data });
       setError(null);
     } catch (err) {
-      setError(err.message);
+      console.error(err);
+      setError(err);
     } finally {
       setIsLoading(false);
     }
   };
 
-  return { login, isLoading, error };
+  return { deleteAccount, error, isLoading };
 };
---
+++ src/pages/UserSettings/DeleteUser.js
@@ -0,0 +1,48 @@
+import { useUser } from "../../hooks/useUser";
+import { useState } from "react";
+import { useDeleteAccount } from "../../hooks/useDeleteAccount";
+
+const DeleteUser = () => {
+  const { user } = useUser();
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState(user.Email);
+  const { deleteAccount, error, isLoading } = useDeleteAccount();
+
+  const handleDelete = () => {
+    console.log("deleting");
+    console.log(password && email);
+    deleteAccount(user.Email, password, user.id);
+  };
+  return (
+    <div className="DeleteUser">
+      <div className="input--container">
+        <label>Email: </label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="input--container">
+        <label>Password: </label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <span className="warning">
+        Warning! This action is irreversible and cannot be undone. All your
+        data, including personal information, settings, and preferences, will be
+        permanently lost.
+      </span>
+      <div className="btn--container">
+        <button className="delete" onClick={handleDelete}>
+          Delete User
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteUser;
