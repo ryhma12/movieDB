5b21f528824b91ac01cf4ea954831c3e68c70aca
jessekl <jesse.klint.jk@gmail.com>
2024-12-01 02:35:26 +0200

    queries and critical db changes

---
+++ server/models/Favourites.js
@@ -0,0 +1,15 @@
+import { pool } from '../helper/db.js'
+
+const selectAllFavourites = async (userId) => {
+    return await pool.query('SELECT * FROM public."Favourites" WHERE "userId" = $1', [userId])
+}
+
+const insertFavourite = async (movieId, movieName, userId) => {
+    return await pool.query('INSERT INTO public."Favourites" ("movieId", "movieName", "userId") VALUES ($1, $2, $3) returning *', [movieId, movieName, userId]);
+}
+
+const removeFavourite = async (id, userId) => {
+    return await pool.query('DELETE FROM public."Favourites" WHERE id = $1 AND "userId" = $2', [id, userId]);
+};
+
+export { selectAllFavourites, insertFavourite, removeFavourite }
