9244b2b1ec43ae0bc2b431241e03c28263b828bc
Eelis-Jus <t3juee01@students.oamk.fi>
2024-12-01 19:50:53 +0200

    updated return on query

--- server/models/User.js
+++ server/models/User.js
@@ -12,7 +12,7 @@ const selectUserByEmail = async (email) => {
 };
 
 const deleteuser = async (email, password) => {
-  return await pool.query('with first_insert as ( select id from "user" where "Email"=$1 and "Password"=$2),second_insert as(select "groupId" from "Role" where "userId"=(select id from first_insert)),third_insert as(delete from "group" where id = (select "groupId" from second_insert)),fourth_insert as (delete from "chat" where "accId"=(select id from first_insert)),fifth_insert as (delete from "Role" where "userId" = (select id from first_insert)),sixth_insert as (delete from "Favourites" where "userId" = (select id from first_insert)),seventh_insert as (delete from "review" where "id"=(select id from first_insert))delete from "user" where id=(select id from first_insert) Returning "Email","Name"',[email,password])
+  return await pool.query('with first_insert as ( select id from "user" where "Email"=$1 and "Password"=$2),second_insert as(select "groupId" from "Role" where "userId"=(select id from first_insert)),third_insert as(delete from "group" where id = (select "groupId" from second_insert)),fourth_insert as (delete from "chat" where "accId"=(select id from first_insert)),fifth_insert as (delete from "Role" where "userId" = (select id from first_insert)),sixth_insert as (delete from "Favourites" where "userId" = (select id from first_insert)),seventh_insert as (delete from "review" where "id"=(select id from first_insert))delete from "user" where id=(select id from first_insert)',[email,password])
 }
 
 export { insertUser, selectUserByEmail, deleteuser };
