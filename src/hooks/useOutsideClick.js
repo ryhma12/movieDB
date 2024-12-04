637eec944af8844bad3d024685a83c83ab4669e3
jessekl <jesse.klint.jk@gmail.com>
2024-12-05 01:47:09 +0200

    useOutsideClick made more readable

--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -3,10 +3,9 @@ import { useEffect } from "react";
 const useOutsideClick = (refs, callback) => {
   useEffect(() => {
     const handleClick = (e) => {
-      const isOutside = refs.every((ref) => {
-        return !(ref.current && ref.current.contains(e.target));
-      });
-
+      const refContainsTarget = (ref) => ref.current && ref.current.contains(e.target);
+      const isOutside = refs.every((ref) => !refContainsTarget(ref));
+      
       // if refs do not contain what is clicked, the callback is run.
       if (isOutside) callback();
     };
