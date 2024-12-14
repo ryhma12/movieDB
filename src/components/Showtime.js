213a4f5e45f304ca246616b8ec7d058cabab8792
Nebul4s <j.hiltula24@gmail.com>
2024-12-14 12:03:53 +0200

    Incorporated tmdb to showtimes and major styling improvements

--- src/components/Showtime.js
+++ src/components/Showtime.js
@@ -1,24 +1,92 @@
-const Showtime = ({ data: {movie = "", time, place, spokenLang = "", subLang1 = "", subLang2 = ""} }) => {
+import useFetch from "../hooks/useFetch";
+import Loading from "./utility/Loading";
+import { ReactComponent as Star } from "../assets/star.svg";
+
+const Showtime = ({
+  data: {
+    movie = "",
+    time,
+    place,
+    spokenLang = "",
+    subLang1 = "",
+    subLang2 = "",
+  },
+}) => {
   const date = new Date(time);
+
+  // Format the date (e.g., "December 14")
   const formattedDate = new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
     month: "long",
     day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    hour12: true,
   }).format(date);
+
+  // Format the time in 24-hour format (e.g., "18:30")
+  const formattedTime = new Intl.DateTimeFormat("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  }).format(date);
+
+  const { data, error, isLoading } = useFetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${
+      process.env.REACT_APP_API_KEY
+    }&query=${movie.replace(" ", "+")}`
+  );
+
+  console.log(data);
 
   return (
     <li className="Showtime">
-      <p className="time">{formattedDate}</p>
-      <p className="place">{place}</p>
-      <p className="movie">{movie}</p>
-      <div className="lang">
-        <p >{spokenLang ? `Language: ${spokenLang}` : null}</p>
-        <p >{subLang1 ? `Sub 1: ${subLang1}` : null}</p>
-        <p >{subLang2 ? `Sub 2: ${subLang2}` : null}</p>
-      </div>
+      {isLoading && <Loading />}
+      {!isLoading && (
+        <>
+          {data.results && data.results.length > 0 && (
+            <img
+              src={`https://image.tmdb.org/t/p/w500${
+                data.results[0].poster_path || ""
+              }`}
+              alt={`${data.results[0].original_title || "Movie"} Poster`}
+              className="movie--poster"
+            />
+          )}
+          <div className="movie--texts">
+            <div className="header">
+              <div className="title--container">
+                <p className="place">{place}</p>
+                <p className="movie">{movie}</p>
+              </div>
+              <div className="lang">
+                <p>{spokenLang && `Language: ${spokenLang}`}</p>
+                <p>{subLang1 && `Sub 1: ${subLang1}`}</p>
+                <p>{subLang2 && `Sub 2: ${subLang2}`}</p>
+              </div>
+            </div>
+            <div className="movie--details__container">
+              {data.results && data.results.length > 0 && (
+                <>
+                  <span className="overview">
+                    {data.results[0].overview || "No overview available."}
+                  </span>
+                  <div className="rating">
+                    <span>
+                      Rating:{" "}
+                      {data.results[0].vote_average
+                        ? data.results[0].vote_average.toFixed(1)
+                        : "N/A"}{" "}
+                      / 10
+                    </span>
+                    {data.results[0].vote_average && <Star />}
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+          <div className="time--container">
+            <p className="time">{formattedTime}</p>
+            <p className="date">{formattedDate}</p>
+          </div>
+        </>
+      )}
     </li>
   );
 };
